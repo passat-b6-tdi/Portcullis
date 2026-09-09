9f53d5b6284567ab0ebf92a55bb731d08637d7ea
Bohdan Pukhno <pukhnobg@gmail.com>
2026-09-10 00:07:59 +0300

    test: arc

--- test/unit/ArcSettlementReceiver.t.sol
+++ test/unit/ArcSettlementReceiver.t.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.36;
 
-import { Test } from "forge-std/Test.sol";
 import { PortcullisGuard } from "../../contracts/core/PortcullisGuard.sol";
 import { ArcSettlementReceiver } from "../../contracts/adapters/ArcSettlementReceiver.sol";
 import { SettlementMessage } from "../../contracts/core/types/GuardTypes.sol";
 import { AddressBookRegistry } from "../../contracts/identity/AddressBookRegistry.sol";
 import { MockERC20 } from "../mocks/MockERC20.sol";
 import { MockRelayer } from "../mocks/MockRelayer.sol";
+import { GuardScenario } from "../util/GuardScenario.sol";
 
-contract ArcSettlementReceiverTest is Test {
+contract ArcSettlementReceiverTest is GuardScenario {
     PortcullisGuard internal guard;
     AddressBookRegistry internal registry;
     ArcSettlementReceiver internal receiver;
@@ -17,9 +17,7 @@ contract ArcSettlementReceiverTest is Test {
     MockERC20 internal usdc;
 
     address internal guardianAddr = makeAddr("guardian");
-    address internal sender = makeAddr("sender");
     address internal recipient = makeAddr("recipient");
-    address internal attacker = makeAddr("attacker");
 
     bytes32 internal constant SRC = keccak256("acme.portcullis.eth");
 
@@ -28,68 +26,76 @@ contract ArcSettlementReceiverTest is Test {
 
     function setUp() public {
         registry = new AddressBookRegistry(address(this));
-        registry.setAuthority(SRC, sender);
+        registry.setAuthority(SRC, authority);
 
         guard = new PortcullisGuard(guardianAddr, guardianAddr, address(registry));
         usdc = new MockERC20("USD Coin", "USDC", 6);
 
-        vm.startPrank(guardianAddr);
-        guard.setBounds(1e6, 5_000_000e6);
-        guard.setAllowedToken(address(usdc), true);
-        vm.stopPrank();
-
         receiver = new ArcSettlementReceiver(guard);
         relayer = new MockRelayer();
         usdc.mint(address(receiver), 1_000_000e6);
+
+        vm.startPrank(guardianAddr);
+        guard.setBounds(1e18, 5_000_000e18); // 18-dec normalised
+        guard.setAllowedToken(address(usdc), true, 6);
+        guard.setEnrolled(SRC, true);
+        guard.setAdapter(address(receiver), true);
+        vm.stopPrank();
     }
 
-    function _msg(uint256 value, uint256 nonce, bytes32 id) internal view returns (SettlementMessage memory) {
+    function _msg(uint256 value, uint256 nonce) internal view returns (SettlementMessage memory) {
         return SettlementMessage({
             srcId: SRC,
-            sender: sender,
             recipient: recipient,
             token: address(usdc),
             value: value,
             appNonce: nonce,
-            messageId: id
+            deadline: block.timestamp + 1 days
         });
     }
 
-    function test_clearedSettlement_paysRecipient() public {
-        vm.expectEmit(true, true, false, true, address(receiver));
-        emit SettlementPaid(keccak256("s1"), recipient, address(usdc), 250_000e6);
+    function _deliver(uint256 value, uint256 nonce) internal {
+        SettlementMessage memory m = _msg(value, nonce);
+        relayer.send(receiver, m, _sign(guard, m));
+    }
 
-        relayer.send(receiver, _msg(250_000e6, 1, keccak256("s1")), "");
+    function test_clearedSettlement_paysRecipient() public {
+        SettlementMessage memory m = _msg(250_000e6, 1);
+
+        vm.expectEmit(true, true, false, true, address(receiver));
+        emit SettlementPaid(guard.digestOf(m), recipient, address(usdc), 250_000e6);
+
+        relayer.send(receiver, m, _sign(guard, m));
 
         assertEq(usdc.balanceOf(recipient), 250_000e6);
         assertEq(usdc.balanceOf(address(receiver)), 750_000e6);
         assertFalse(guard.paused());
     }
 
-    function test_hostileSettlement_notPaid_andTrips() public {
-        SettlementMessage memory m = _msg(100e6, 1, keccak256("s1"));
-        m.sender = attacker;
+    function test_unauthenticatedSettlement_notPaid_notLatched() public {
+        SettlementMessage memory m = _msg(100e6, 1);
 
         vm.expectEmit(true, false, false, false, address(receiver));
-        emit SettlementRejected(m.messageId);
+        emit SettlementRejected(guard.digestOf(m));
 
-        relayer.send(receiver, m, "");
+        relayer.send(receiver, m, _signAs(guard, m, 0xBEEF));
 
         assertEq(usdc.balanceOf(recipient), 0);
-        assertTrue(guard.paused());
+        assertFalse(guard.paused());
     }
 
     function test_insufficientFloat_reverts_andNoLedgerMutation() public {
+        SettlementMessage memory m = _msg(2_000_000e6, 1);
         vm.expectRevert();
-        relayer.send(receiver, _msg(2_000_000e6, 1, keccak256("s1")), "");
+        relayer.send(receiver, m, _sign(guard, m));
 
         assertEq(guard.lastNonce(SRC), 0);
-        assertFalse(guard.seen(keccak256("s1")));
+        assertFalse(guard.seen(guard.digestOf(m)));
     }
 
     function test_sequentialSettlements() public {
-        relayer.send(receiver, _msg(10e6, 1, keccak256("a")), "");
-        relayer.send(receiver, _msg(20e6, 2, keccak256("b")), "");
+        _deliver(10e6, 1);
+        _deliver(20e6, 2);
         assertEq(usdc.balanceOf(recipient), 30e6);
         assertEq(guard.lastNonce(SRC), 2);
     }
