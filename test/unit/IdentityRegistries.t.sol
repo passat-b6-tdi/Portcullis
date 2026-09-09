e551b9730a1c461ff803d50dd4beac2083393053
Bohdan Pukhno <pukhnobg@gmail.com>
2026-09-10 00:07:08 +0300

    feat: upd identity registers

--- test/unit/IdentityRegistries.t.sol
+++ test/unit/IdentityRegistries.t.sol
@@ -71,39 +71,74 @@ contract EnsIdentityRegistryTest is Test {
     MockEnsResolver internal resolver;
     EnsIdentityRegistry internal registry;
 
+    address internal owner = makeAddr("owner");
+    address internal stranger = makeAddr("stranger");
     bytes32 internal constant NODE = keccak256("treasury.acme.portcullis.eth");
     address internal authority = makeAddr("authority");
 
     function setUp() public {
         ens = new MockEnsRegistry();
         resolver = new MockEnsResolver();
-        registry = new EnsIdentityRegistry(address(ens));
+        registry = new EnsIdentityRegistry(address(ens), owner);
+
+        ens.setResolver(NODE, address(resolver));
+        resolver.setAddr(NODE, authority);
+    }
+
+    function _allow(bytes32 node) internal {
+        vm.prank(owner);
+        registry.setAllowed(node, true);
     }
 
     function test_constructor_rejectsZeroEns() public {
         vm.expectRevert(AddressHelper.ZeroAddress.selector);
-        new EnsIdentityRegistry(address(0));
+        new EnsIdentityRegistry(address(0), owner);
     }
 
-    function test_noResolver_resolvesZero() public view {
+    function test_constructor_rejectsZeroOwner() public {
+        vm.expectRevert(AddressHelper.ZeroAddress.selector);
+        new EnsIdentityRegistry(address(ens), address(0));
+    }
+
+    function test_setAllowed_onlyOwner() public {
+        vm.prank(stranger);
+        vm.expectRevert(Ownable.Unauthorized.selector);
+        registry.setAllowed(NODE, true);
+    }
+
+    function test_notAllowlisted_resolvesZero() public view {
         assertEq(registry.resolve(NODE), address(0));
     }
 
-    function test_resolverSet_returnsAddr() public {
-        ens.setResolver(NODE, address(resolver));
-        resolver.setAddr(NODE, authority);
+    function test_allowlistedWithRecord_returnsAddr() public {
+        _allow(NODE);
         assertEq(registry.resolve(NODE), authority);
     }
 
+    function test_noResolver_resolvesZero() public {
+        bytes32 other = keccak256("ops.acme.portcullis.eth");
+        _allow(other);
+        assertEq(registry.resolve(other), address(0));
+    }
+
     function test_resolverReverts_resolvesZero() public {
-        ens.setResolver(NODE, address(resolver));
-        resolver.setAddr(NODE, authority);
+        _allow(NODE);
         resolver.setRevert(true);
         assertEq(registry.resolve(NODE), address(0));
     }
 
     function test_resolverSetButNoRecord_resolvesZero() public {
-        ens.setResolver(NODE, address(resolver));
-        assertEq(registry.resolve(NODE), address(0));
+        bytes32 blank = keccak256("blank.acme.portcullis.eth");
+        ens.setResolver(blank, address(resolver));
+        _allow(blank);
+        assertEq(registry.resolve(blank), address(0));
+    }
+
+    function test_setAllowedBatch_togglesMany() public {
+        bytes32[] memory nodes = new bytes32[](1);
+        nodes[0] = NODE;
+        vm.prank(owner);
+        registry.setAllowedBatch(nodes, true);
+        assertEq(registry.resolve(NODE), authority);
     }
 }
