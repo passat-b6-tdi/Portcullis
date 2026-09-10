a11280ed335cbfca4c5b1b1ffa81c67c29d9f5be
Bohdan Pukhno <pukhnobg@gmail.com>
2026-09-10 10:43:21 +0300

    test: upd ens mock

--- test/mocks/MockEns.sol
+++ test/mocks/MockEns.sol
@@ -1,28 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.36;
 
-contract MockEnsResolver {
+contract MockUniversalResolver {
+    error ResolverNotFound(bytes name);
+    error OffchainLookup(address sender, string[] urls, bytes callData, bytes4 callbackFunction, bytes extraData);
+
+    enum Mode {
+        Ok,
+        ResolverMissing,
+        ResolverDown,
+        Offchain,
+        EmptyResult
+    }
+
+    Mode public mode;
     mapping(bytes32 => address) internal _addr;
-    bool public doRevert;
+    mapping(bytes32 => bool) internal _hasResolver;
 
     function setAddr(bytes32 node, address a) external {
         _addr[node] = a;
+        _hasResolver[node] = true;
     }
 
-    function setRevert(bool v) external {
-        doRevert = v;
+    function setHasResolver(bytes32 node, bool v) external {
+        _hasResolver[node] = v;
     }
 
-    function addr(bytes32 node) external view returns (address) {
-        require(!doRevert, "resolver down");
-        return _addr[node];
+    function setMode(Mode m) external {
+        mode = m;
     }
-}
 
-contract MockEnsRegistry {
-    mapping(bytes32 => address) public resolver;
+    function resolve(bytes calldata name, bytes calldata data) external view returns (bytes memory, address) {
+        if (mode == Mode.ResolverDown) revert("resolver down");
+        if (mode == Mode.Offchain) {
+            revert OffchainLookup(address(this), new string[](0), data, bytes4(0), "");
+        }
+        if (mode == Mode.EmptyResult) return (hex"", address(this));
 
-    function setResolver(bytes32 node, address r) external {
-        resolver[node] = r;
+        bytes32 node = abi.decode(data[4:], (bytes32));
+        if (mode == Mode.ResolverMissing || !_hasResolver[node]) revert ResolverNotFound(name);
+        return (abi.encode(_addr[node]), address(this));
     }
 }
